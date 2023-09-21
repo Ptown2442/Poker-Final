7c726665b204a8127e8a4129d5a626201c14c9d7
Ptown2442 <portervaughn93@gmail.com>
2023-09-20 19:15:12 -0700

    grid established home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import PokerTable from "./components/PokerTable";
+import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
 
 export default function App() {
   return (
     <>
       <ChakraProvider>
-        <div>Hell's Kitchen</div>
-        <PokerTable></PokerTable>
+        <Grid
+          templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+          gridTemplateRows={"50px 1fr 30px"}
+          gridTemplateColumns={"150px 1fr"}
+          h="200px"
+          gap="1"
+          color="blackAlpha.700"
+          fontWeight="bold"
+        >
+          <GridItem pl="2" bg="orange.300" area={"header"}>
+            Header
+          </GridItem>
+          <GridItem pl="2" bg="pink.300" area={"nav"}>
+            Nav
+          </GridItem>
+          <GridItem pl="2" bg="green.300" area={"main"}>
+            Main
+          </GridItem>
+          <GridItem pl="2" bg="blue.300" area={"footer"}>
+            Footer
+          </GridItem>
+        </Grid>
       </ChakraProvider>
     </>
   );
